bc04ac13c2ac5fa28cacfc74b190adaab592c1d6
andherpaulo <andher.dev@gmail.com>
2023-01-25 21:54:22 -0300

    feat: add get /tickets routes

--- src/services/tickets-service/index.ts
+++ src/services/tickets-service/index.ts
@@ -1,6 +1,13 @@
+import { notFoundError } from "@/errors";
 import ticketRepository, { CreateTicketParams } from "@/repositories/ticket-repository";
 import { Ticket, TicketType } from "@prisma/client";
 import enrollmentsService from "../enrollments-service";
+
+export type CreateTicket = Omit<CreateTicketParams, "enrollmentId">;
+
+export type TicketWithTicketType = Ticket & {
+  TicketType: TicketType;
+};
 
 async function createTicket(params: CreateTicket, userId: number): Promise<TicketWithTicketType> {
   const enrollment = await enrollmentsService.getOneWithAddressByUserId(userId);
@@ -9,19 +16,26 @@ async function createTicket(params: CreateTicket, userId: number): Promise<Ticke
 
   const createdTicket = await ticketRepository.create({ ticketTypeId, enrollmentId: enrollment.id });
 
-  const TicketType = await ticketRepository.findTicketTypeById(ticketTypeId);
+  const createdTicketWithTicketTypeById = (await ticketRepository.findTicketWithTicketTypeById(
+    createdTicket.id,
+  )) as TicketWithTicketType;
 
-  return { ...createdTicket, TicketType };
+  return createdTicketWithTicketTypeById;
 }
 
-export type CreateTicket = Omit<CreateTicketParams, "enrollmentId">;
+async function getTicketByUserId(userId: number): Promise<TicketWithTicketType> {
+  const enrollment = await enrollmentsService.getOneWithAddressByUserId(userId);
 
-export type TicketWithTicketType = Ticket & {
-  TicketType: TicketType;
-};
+  const ticket = await ticketRepository.findTicketTypeById(enrollment.id);
+
+  if (!ticket) throw notFoundError("Ticket");
+
+  return ticket;
+}
 
 const ticketsService = {
   createTicket,
+  getTicketByUserId,
 };
 
 export default ticketsService;
